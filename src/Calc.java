2efa8867e8080b7b5a951f8d8da92f5b4e13e99e
admin <admin@admin-HP>
2018-06-12 14:53:42 +0900

    Hello World

---
+++ src/Calc.java
@@ -0,0 +1,11 @@
+
+public class Calc {
+
+	public static void main(String[] args) {
+//		System.out.println("加算の結果は"+ (3 + 2) + "です。");
+//		System.out.println("減算の結果は" - 8 + "です。");
+//		System.out.println("乗算の結果は" * 4 + "です。");
+//		System.out.println("除算の結果は" / 2 + "です。");
+		
+	}
+}
